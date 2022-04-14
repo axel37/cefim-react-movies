d2dc2a5d2c507a7bdac4de5570a32d4978848ebd
Axel Choteau <axelc37@outlook.fr>
2022-04-14 17:26:49 +0200

    "Show More" button on movie synopsis

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -4,7 +4,16 @@ import Gallery from "./Gallery";
 
 class Movie extends React.Component
 {
-   /*
+
+    constructor(props, context)
+    {
+        super(props, context);
+        this.state = {
+            "isShortSynopsis": true
+        };
+    }
+
+    /*
    * Composant Movie
    * Un film contiendra au moins un titre, une année de sortie, un réalisateur et
    * un synopsis
@@ -13,6 +22,7 @@ class Movie extends React.Component
     {
         // Put props into variables
         const {posterUrl, title, year, director, synopsis} = this.props;
+        const {isShortSynopsis} = this.state;
 
         return (
             <article className="Movie">
@@ -20,9 +30,18 @@ class Movie extends React.Component
                 <h2>{title}</h2>
                 <time>{year}</time>
                 <p>{director}</p>
-                <p className="synopsis">{synopsis}</p>
+                <p className="synopsis">{isShortSynopsis ? synopsis.substring(0, 50) + '...' : synopsis}</p>
+                {
+                    isShortSynopsis && <button onClick={this.showMore}>Lire Plus</button>
+                }
             </article>
         );
+    }
+
+    showMore = () => {
+        this.setState({
+            "isShortSynopsis": false
+        });
     }
 }
 
